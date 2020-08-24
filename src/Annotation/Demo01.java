c73e7256ff68b9fb81bfdc9bbf6061f26ceb5089
zhaijingfu <zhaiblackhool@gmail.com>
2020-08-24 23:54:11 +0800

    Annotation

---
+++ src/Annotation/Demo01.java
@@ -0,0 +1,22 @@
+package Annotation;
+
+/**
+ * @program: JavaTest
+ * @description: ${description}
+ * @author: Mr.ZeroCamel
+ * @create: 2020-08-24 22:44
+ **/
+public class Demo01 {
+    public static void main(String[] args)
+    {
+
+    }
+
+    @MyAnnotation
+    @MyAnnotation01(value = "ddd")
+    public void test01()
+    {
+
+    }
+
+}
---
+++ src/Annotation/MyAnnotation01.java
@@ -0,0 +1,19 @@
+package Annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @program: JavaTest
+ * @description: ${自定义注解}
+ * @author: Mr.ZeroCamel
+ * @create: 2020-08-24 22:40
+ **/
+@Target(value = {ElementType.METHOD,ElementType.TYPE})
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface MyAnnotation01 {
+    // 参数 参数值 默认值
+    String value() default "";
+}
